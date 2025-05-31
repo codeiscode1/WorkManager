231a462789da08eb8b7c13333b3939caf997a50a
codeiscode1 <s.tanav9542@gmail.com>
2025-05-31 17:21:11 +0530

    Update diary.py
    Update diary.py

--- src/diary.py
+++ src/diary.py
@@ -1,10 +1,22 @@
 from tkinter import Tk,Toplevel,Label
 from datetime import datetime
-from pandas import DataFrame,read_csv
+from pandas import DataFrame,read_csv,errors
 from tkcalendar import DateEntry
 
 app = Tk()
 
 def diary(tkinter_app):
+    try :
+        diary_entries = read_csv("diary.csv").to_dict(orients = "records")
+    except FileNotFoundError :
+        with open("diary.csv", "w" ) as f :
+            f.close()
+    except errors.EmptyDataError :
+        diary_entries = []
     app_diary = Toplevel(tkinter_app)
     a = Label(app_diary,text="Welcome to your diary.")
+    a.grid(row = 0, column = 0)
+    diary_date_entry_text = Label(app_diary,text="Please Enter Date:")
+    diary_date_entry_text.grid(row=1,column=0)
+    diary_date_entry = DateEntry(app_diary)
+    diary_date_entry.grid(row=1,column=0)
