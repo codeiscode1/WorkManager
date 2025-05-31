d2716dbf76c747e9f9453f89a8c0d12dc6343396
codeiscode1 <s.tanav9542@gmail.com>
2025-05-31 17:04:20 +0530

    Minor Update

--- src/diary.py
+++ src/diary.py
@@ -1,9 +1,10 @@
-from tkinter import *
+from tkinter import Tk,Toplevel,Label
 from datetime import datetime
 from pandas import DataFrame,read_csv
+from tkcalendar import DateEntry
 
 app = Tk()
 
-def diary(tkinter_app) :
+def diary(tkinter_app):
     app_diary = Toplevel(tkinter_app)
-    
+    a = Label(app_diary,text="Welcome to your diary.")
